dd34b21e6d811c6d4f4bd256c5e1c6f393628f3d
Linda <fv19959@bristol.ac.uk>
2020-11-26 15:25:53 +0000

    branch?

--- hello_world.go
+++ hello_world.go
@@ -10,22 +10,7 @@ const s string = "constant"
 func main() {
 	fmt.Println("hello world")
 	fmt.Println("go" + " lang")
-	fmt.Println(true && false)
-
-	var a = "initial"
-	fmt.Println(a)
-
-	var b, c int = 1, 2
-	fmt.Println(b, c)
-
-	var d = true
-	fmt.Println(d)
-
-	var e int
-	fmt.Println(e)
-
-	f := "apple"
-	fmt.Println(f)
+)
 
 	fmt.Println(s)
 	const n = 240000
